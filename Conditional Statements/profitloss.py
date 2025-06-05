42b5da91c654de5f68449481158d87eadaf38af9
ARADHYA108 <aradhya6490@gmail.com>
2025-06-05 12:44:30 +0530

    Conditional Statements

---
+++ Conditional Statements/profitloss.py
@@ -0,0 +1,8 @@
+cost_price = 100
+selling_price = 120
+
+if (selling_price > cost_price):
+    print("You have a profit of rupees" , selling_price - cost_price)
+
+else:
+    print("You have a loss of" , cost_price - selling_price , "Rupees")    
