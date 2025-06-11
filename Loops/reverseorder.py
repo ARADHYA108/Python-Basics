206830f3cdaa84f65785380d1d48ce58086cc162
ARADHYA108 <aradhya6490@gmail.com>
2025-06-11 12:47:18 +0530

    Loops

---
+++ Loops/reverseorder.py
@@ -0,0 +1,8 @@
+num = int (input("Enter the number."))
+
+i = num
+
+while i >= 1:
+    print(i)
+    i -= 1
+print("Reverse order is " , i)
---
+++ Loops/wholeno_sum.py
@@ -0,0 +1,10 @@
+num = int (input("Number of terms you want to add"))
+
+i = 0
+
+sum = 0 
+
+while i < num:
+    sum = sum + i 
+    i += 1 
+print("Sum of whole numbers is " , sum)    
