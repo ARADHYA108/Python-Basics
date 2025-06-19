fbf0f5568e5b306150b40c2f75aca74f0d965fff
ARADHYA108 <aradhya6490@gmail.com>
2025-06-19 11:30:57 +0530

    After Class Project

---
+++ After Class Project/Intermediate Level/patterns.py
@@ -0,0 +1,12 @@
+print("Mirrored Right Triangle Of Stars ( * ):")
+
+rows =  int (input(" Enter the no. of rows you want : ")) 
+
+
+
+for i in range  ( 1 , rows + 1 ):
+
+
+    print( ' ' * ( rows - i) + '*' * i)
+
+    
